b7834e62dc6f88e7e8a6a0b5fd9ae15848b0584c
toprhythm <2573424062@qq.com>
2022-02-18 14:23:57 +0800

    完成 筛选区展示

--- src/api/category.js
+++ src/api/category.js
@@ -15,3 +15,11 @@ export const findAllCategory = () => {
 export const findTopCategory = (id) => {
   return request('/category', 'get', { id })
 }
+
+/**
+ * 获取二级类目下的筛选数据
+ * @param {String} id : 二级类目id
+ */
+export const findSubCategoryFilter = (id) => {
+  return request('/category/sub/filter', 'get', { id })
+}
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -2,14 +2,16 @@
   <div>
     <div class="container">
       <SubBread />        <!--面包屑-->
+      <SubFilter />       <!--筛选区-->
     </div>
   </div>
 </template>
 <script>
 import SubBread from './components/sub-bread'
+import SubFilter from './components/sub-filter'
 export default {
   name: 'SubCategory',
-  components: { SubBread }
+  components: { SubBread, SubFilter }
 }
 </script>
 <style scoped lang="less"></style>
