875ad91c1c6dc09d76ce9926233bf9a805832217
toprhythm <2573424062@qq.com>
2022-02-18 09:15:45 +0800

    完成01-顶级类目-面包屑组件-初级

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -6,6 +6,7 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import XtxBread from './xtx-bread.vue'
 import defaultImg from '@/assets/images/200.png'
 
 export default {
@@ -15,6 +16,7 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    app.component(XtxBread.name, XtxBread)
     // 定义指令
     defineDirective(app)
   }
---
+++ src/components/library/xtx-bread.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='xtx-bread'>
+    <div class="xtx-bread-item">
+      <RouterLink to="/">首页</RouterLink>
+    </div>
+    <i class="iconfont icon-angle-right"></i>
+    <div class="xtx-bread-item" v-if="parentName">
+      <RouterLink v-if="parentPath" :to="parentPath">{{parentName}}</RouterLink>
+      <span v-else>{{parentName}}</span>
+    </div>
+    <i v-if="parentName" class="iconfont icon-angle-right"></i>
+    <div class="xtx-bread-item">
+      <span><slot /></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XtxBread',
+  props: {
+    parentPath: { // 父亲类目的路径
+      type: [String, Object],
+      default: '/'
+    },
+    parentName: { // 父亲类目的名称
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+  .xtx-bread{
+    display: flex;
+    padding: 25px 10px;
+    &-item {
+      a {
+        color: #666;
+        transition: all .4s;
+        &:hover {
+          color: @xtxColor;
+        }
+      }
+    }
+    i {
+      font-size: 12px;
+      margin-left: 5px;
+      margin-right: 5px;
+      line-height: 22px;
+    }
+  }
+</style>
