4cda59163b825ae3247e6d5245339441b6c6c150
toprhythm <2573424062@qq.com>
2022-02-18 10:26:30 +0800

    完成 02-顶级类目-面包屑组件-高级

--- src/components/library/xtx-bread-item.vue
+++ src/components/library/xtx-bread-item.vue
@@ -2,7 +2,6 @@
   <div class="xtx-bread-item">
     <RouterLink v-if="to" :to="to"><slot /></RouterLink>
     <span v-else><slot /></span>
-    <i class="iconfont icon-angle-right"></i>
   </div>
 </template>
 <script>
--- src/components/library/xtx-bread.vue
+++ src/components/library/xtx-bread.vue
@@ -1,12 +1,30 @@
-<template>
-  <div class='xtx-bread'>
-    <slot />
-  </div>
-</template>
-
 <script>
+import { h } from 'vue'
 export default {
-  name: 'XtxBread'
+  name: 'XtxBread',
+  render () {
+    // 1. template标签去除，单文件组件
+    // 2. render返回值就是组件内容
+    // 3. vue2.0 的h函数传参进来的，但是vue3.0的 h 函数是需要导入进来的
+    // 4. h的第一个参数，标签名，第二个参数，标签的属性对象，第三个参数：子节点
+    // 需求
+    // 1. 创建xtx-bread父容器
+    // 2. 获取默认插槽内容
+    // 3. 去除xtx-bread-item组件的i标签,应该由render函数来组织
+    // 4. 遍历插槽中的item, 得到一个动态创建的节点, 最后一个item不加i标签
+    // 5. 把动态创建的节点渲染在xtx-bread标签中
+    const items = this.$slots.default()// 拿到所有xtx-bread-item
+    // console.log(items)
+    const dymanicItems = []
+    items.forEach((item, i) => {
+      dymanicItems.push(item)
+      if (i < (items.length - 1)) {
+        dymanicItems.push(h('i', { class: 'iconfont icon-angle-right' }))
+      }
+    })
+    // return h('div', { class: 'xtx-bread' }, '子节点')
+    return h('div', { class: 'xtx-bread' }, dymanicItems)
+  }
 }
 </script>
 
