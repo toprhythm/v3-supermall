ca44ac3eaac8527fb26efe1e67e70d7a6ba8090f
toprhythm <2573424062@qq.com>
2022-02-18 13:23:40 +0800

    完成展示面包屑，之前的那个是处理跳转细节

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Sub-Category</div>
+  <div>
+    <div class="container">
+      <SubBread />        <!--面包屑-->
+    </div>
+  </div>
 </template>
 <script>
+import SubBread from './components/sub-bread'
 export default {
-  name: 'SubCategory'
+  name: 'SubCategory',
+  components: { SubBread }
 }
 </script>
 <style scoped lang="less"></style>
